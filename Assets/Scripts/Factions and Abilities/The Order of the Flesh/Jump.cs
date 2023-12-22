6d8606a9fd94d78acaa2d18aa028adbb3538adca
krizantemum <yurekce.altin@metu.edu.tr>
2023-12-22 18:53:53 +0200

    Jump Functionality
    No glide

--- Assets/Scripts/Factions and Abilities/The Order of the Flesh/Jump.cs
+++ Assets/Scripts/Factions and Abilities/The Order of the Flesh/Jump.cs
@@ -3,24 +3,19 @@ using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
 
-public class Jump : MonoBehaviour, IPlayerAbility{
+public class Jump : MonoBehaviour, IPlayerAbility
+{
     [SerializeField] private Faction faction;
     [SerializeField] private float raycastDistance = 0.55f;
     [SerializeField] private bool isOnGround;
-    [SerializeField] private bool isGliding;
     [SerializeField] private bool isFalling;
-    [SerializeField] private bool isOnPeakHeight;
     [SerializeField] private bool inputReceived;
-    [SerializeField] private bool firstJumpComplete = false;
     [SerializeField] private float jumpForce = 20;
-    [SerializeField] private float glideGravityMultiplier = 0.3f;
-    [SerializeField] private float jumpTimer = 2;
     // Start is called before the first frame update
-    private void Start() {
+    private void Start()
+    {
         this.faction = GetComponentInParent<Faction>();
     }
-
-    private Vector2 previousFrameVelocity = Vector2.zero;
 
     public Faction PlayerFaction => faction;
 
@@ -34,38 +29,25 @@ public class Jump : MonoBehaviour, IPlayerAbility{
 
     public bool CanBeUsed => true;
 
-    public void UseAbility(bool inputReceived){
+    public void UseAbility(bool inputReceived)
+    {
         RaycastHit2D hitBelow = Physics2D.Raycast(transform.position, -transform.up, raycastDistance, LayerMask.GetMask("ForegroundEnvironment"));
         RaycastHit2D hitAbove = Physics2D.Raycast(transform.position, transform.up, raycastDistance, LayerMask.GetMask("ForegroundEnvironment"));
-        Debug.DrawLine(transform.position, transform.position-(transform.up*raycastDistance), Color.red); //Hit below visualization
-        Debug.DrawLine(transform.position, transform.position+(transform.up*raycastDistance), Color.red); //Hit above visualization
-        isOnGround = hitBelow.collider != null;
-        isOnPeakHeight = hitAbove.collider != null;
-        isGliding = !isOnGround & inputReceived;
+        Debug.DrawLine(transform.position, transform.position - (transform.up * raycastDistance), Color.red); //Hit below visualization
+        Debug.DrawLine(transform.position, transform.position + (transform.up * raycastDistance), Color.red); //Hit above visualization
         isFalling = !isOnGround & !inputReceived;
 
-        if(isOnGround){
-            firstJumpComplete = false;
-            jumpTimer = 2;
+        if (inputReceived)
+        {
+            faction.player.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2(0, jumpForce));
         }
-        if(inputReceived && !firstJumpComplete){ //Jump
-            jumpTimer = jumpTimer >= 0 ? jumpTimer*0.95f : 0;
-            faction.player.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce*jumpTimer));
+        else if (isFalling)
+        {
+            faction.player.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -jumpForce));
         }
-        else if((!inputReceived && !isOnGround) || isOnPeakHeight){ 
-            firstJumpComplete = true;
-        }
-        if(isOnPeakHeight){
-            firstJumpComplete = true;
-        }
-        
-        if(firstJumpComplete && inputReceived && faction.player.GetComponent<Rigidbody2D>().velocity.y < 0){ //Glide
-            faction.player.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, glideGravityMultiplier * jumpForce));
-        }
-        previousFrameVelocity = faction.player.GetComponent<Rigidbody2D>().velocity;  
     }
-
-    public void ResetCooldown(){
+    public void ResetCooldown()
+    {
         return;
     }
 }
