6405a805d86a673f6c2f1ec028cb4ab0e64d0de0
Serhan <35522745+SerhanTRGL@users.noreply.github.com>
2023-12-24 19:20:57 +0200

    Simplified Jump.cs

--- Assets/Scripts/Factions and Abilities/The Order of the Flesh/Jump.cs
+++ Assets/Scripts/Factions and Abilities/The Order of the Flesh/Jump.cs
@@ -1,55 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Threading;
 using UnityEngine;
 
 public class Jump : MonoBehaviour, IPlayerAbility{
     [SerializeField] private Faction faction;
-    [SerializeField] private float raycastDistance = 0.55f;
-    [SerializeField] private bool isOnGround;
-    [SerializeField] private bool isGliding;
-    [SerializeField] private bool isFalling;
-    [SerializeField] private bool isOnPeakHeight;
-    [SerializeField] private bool inputReceived;
-    [SerializeField] private bool firstJumpComplete = false;
     [SerializeField] private float jumpForce = 20;
-    [SerializeField] private float glideGravityMultiplier = 0.3f;
-    [SerializeField] private float jumpTimer = 2;
-    // Start is called before the first frame update
+    public Faction PlayerFaction => faction;
+    public string AbilityName => "TheOrderOfTheFleshJump";
+    public GameObject AbilityOwner => faction.player.gameObject;
+    public float Cooldown => 0;
+    public float AbilityDuration => 0;
+    public bool CanBeUsed => true;
+    
     private void Start() {
         this.faction = GetComponentInParent<Faction>();
     }
 
-    private Vector2 previousFrameVelocity = Vector2.zero;
-
-    public Faction PlayerFaction => faction;
-
-    public string AbilityName => "TheOrderOfTheFleshJump";
-
-    public GameObject AbilityOwner => faction.player.gameObject;
-
-    public float Cooldown => 0;
-
-    public float AbilityDuration => 0;
-
-    public bool CanBeUsed => true;
-
     public void UseAbility(bool inputReceived){
-        RaycastHit2D hitBelow = Physics2D.Raycast(transform.position, -transform.up, raycastDistance, LayerMask.GetMask("ForegroundEnvironment"));
-        RaycastHit2D hitAbove = Physics2D.Raycast(transform.position, transform.up, raycastDistance, LayerMask.GetMask("ForegroundEnvironment"));
-        Debug.DrawLine(transform.position, transform.position-(transform.up*raycastDistance), Color.red); //Hit below visualization
-        Debug.DrawLine(transform.position, transform.position+(transform.up*raycastDistance), Color.red); //Hit above visualization
-        isOnGround = hitBelow.collider != null;
-        isOnPeakHeight = hitAbove.collider != null;
-        isGliding = !isOnGround & inputReceived;
-        isFalling = !isOnGround & !inputReceived;
-
-        if(isOnGround){
-            firstJumpComplete = false;
-            jumpTimer = 2;
-        }
-        if(inputReceived){ //Jump
-            faction.player.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce*jumpTimer));
+        if(inputReceived){
+            faction.player.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
         } 
     }
 
