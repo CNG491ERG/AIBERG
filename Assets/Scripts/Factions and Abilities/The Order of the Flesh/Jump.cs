534a854f8298386f6c51b5e9a0ed798014e6f118
Serhan <35522745+SerhanTRGL@users.noreply.github.com>
2023-12-24 19:21:18 +0200

    Update Jump.cs

--- Assets/Scripts/Factions and Abilities/The Order of the Flesh/Jump.cs
+++ Assets/Scripts/Factions and Abilities/The Order of the Flesh/Jump.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class Jump : MonoBehaviour, IPlayerAbility{
     [SerializeField] private Faction faction;
-    [SerializeField] private float jumpForce = 20;
+    [SerializeField] private float jumpForce;
     public Faction PlayerFaction => faction;
     public string AbilityName => "TheOrderOfTheFleshJump";
     public GameObject AbilityOwner => faction.player.gameObject;
     public float Cooldown => 0;
     public float AbilityDuration => 0;
     public bool CanBeUsed => true;
-    
+
     private void Start() {
         this.faction = GetComponentInParent<Faction>();
     }
