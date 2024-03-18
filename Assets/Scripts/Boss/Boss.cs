c9994e75fb270fc787a485dda0a3f9083aff0c6a
Serhan <35522745+SerhanTRGL@users.noreply.github.com>
2024-03-18 07:18:37 +0200

    Updated boss.cs and added bossabilitiesSO.cs

--- Assets/Scripts/Boss/Boss.cs
+++ Assets/Scripts/Boss/Boss.cs
@@ -1,62 +1,76 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using Unity.MLAgents.Actuators;
-using Unity.MLAgents;
-using Unity.MLAgents.Sensors;
 using System;
 
-public class Boss : MonoBehaviour, IDamageable
-{
+public class Boss : MonoBehaviour, IDamageable{
+    [Header("IDamageable Values")]
     [SerializeField] private float health;
-    [SerializeField] private float maxHealth = 100;
+    [SerializeField] private float maxHealth;
     [SerializeField] private float defense;
+
+    [Header("Boss Attributes")]
     [SerializeField] private int enragement;
-    [SerializeField] public float speed;
+    [SerializeField] private float speed;
     [SerializeField] private float cooldownMultiplier;
     [SerializeField] private float damageMultiplier;
-    [SerializeField] private Transform targetTransform;
-    [SerializeField] private Rigidbody2D bossRb;
-    private IAbility moveUp;
-    private IAbility moveDown;
-    private IAbility basicAttack;
-    public Player player;
 
+    [Header("Boss Abilities")]
+    [SerializeField] private BossAbilitiesSO bossAbilities;
+    [SerializeField] private GameObject moveUpAbilityObject;
+    [SerializeField] private GameObject moveDownAbilityObject;
+    [SerializeField] private GameObject basicAbilityObject;
+    [SerializeField] private GameObject attackDroneAbilityObject;
+    public IAbility moveUpAbility;
+    public IAbility moveDownAbility;
+    public IAbility basicAbility;
+    public IAbility attackDroneAbility;
+
+    [Header("Other")]
+    public Player player;
     public event EventHandler OnDamageableDeath;
     public event EventHandler OnDamageableHurt;
 
-    public float Health { 
-        get{
-            return health;
-        }
-        set{
-            health = value;
-        }
-    }
-    public float Defense { 
-        get{
-            return defense;
-        }
-        set{
-            defense = value;
-        }
-    }
-
+    public float Health { get => health; set => health = value;}
+    public float Defense { get => defense; set => defense = value;}
     public float MaxHealth { get => maxHealth; set => maxHealth = value; }
 
     private void Start() {
-        //moveUp = GetComponent<MoveUp>();
-        //moveDown = GetComponent<MoveDown>();
-        basicAttack = GetComponent<BasicAttack>();
+        if(bossAbilities.BasicAbility != null){
+            basicAbilityObject = Instantiate(bossAbilities.BasicAbility, this.transform);
+            basicAbility = basicAbilityObject.GetComponent<IAbility>();
+        }
+        if(bossAbilities.MoveUpAbility != null){
+            moveUpAbilityObject = Instantiate(bossAbilities.MoveUpAbility, this.transform);
+            moveUpAbility = moveUpAbilityObject.GetComponent<IAbility>();
+        }
+        if(bossAbilities.MoveDownAbility != null){
+            moveDownAbilityObject = Instantiate(bossAbilities.MoveDownAbility, this.transform);
+            moveDownAbility = moveDownAbilityObject.GetComponent<IAbility>();
+        }
+        if(bossAbilities.AttackDroneAbility != null){
+            attackDroneAbilityObject = Instantiate(bossAbilities.AttackDroneAbility, this.transform);
+            attackDroneAbility = attackDroneAbilityObject.GetComponent<IAbility>();
+        }
+
+        health = maxHealth = bossAbilities.MaxHealth;
+        defense = bossAbilities.Defense;
+        speed = bossAbilities.Speed;
     }
     
     public void TakeDamage(float damageToTake) {
         float totalDamage = damageToTake * (1 - Defense);
         Health = Health - totalDamage <= 0 ? 0 : Health - totalDamage;
         OnDamageableHurt?.Invoke(this, EventArgs.Empty);
+
         if(Health == 0){
             OnDamageableDeath?.Invoke(this, EventArgs.Empty);
         }
+    }
+
+    public void ResetAllCooldowns(){
+        basicAbility?.ResetCooldown();
+        moveUpAbility?.ResetCooldown();
+        moveDownAbility?.ResetCooldown();
+        attackDroneAbility?.ResetCooldown();
     }
 }
     
---
+++ Assets/Scripts/ScriptableObjects/BossAbilitiesSO.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BossAbilities", menuName = "ScriptableObjects/BossAbilitiesScriptableObject", order = 2)]
+public class BossAbilitiesSO : ScriptableObject{
+    public GameObject MoveUpAbility;
+    public GameObject MoveDownAbility;
+    public GameObject BasicAbility;
+    public GameObject AttackDroneAbility;
+    public GameObject Jump;
+    public float MaxHealth;
+    public float Defense;
+    public float Speed;
+}
