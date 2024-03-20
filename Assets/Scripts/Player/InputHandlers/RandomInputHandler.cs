780b25c7eaf24354ed865f864a32dcb949b425db
Serhan <35522745+SerhanTRGL@users.noreply.github.com>
2024-03-20 20:17:30 +0200

    Added random input handler

---
+++ Assets/Scripts/Player/InputHandlers/RandomInputHandler.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class RandomInputHandler : InputHandler{
+    private void FixedUpdate() {
+        this.JumpInput = Random.Range(0f,1f) < 0.40f;
+        this.ActiveAbility1Input = Random.Range(0f,1f) < 0.01f;
+        this.ActiveAbility2Input = Random.Range(0f,1f) < 0.01f;
+        this.BasicAbilityInput = Random.Range(0f,1f) < 0.05f;
+    }
+}
