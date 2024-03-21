d5f94881336f82f667c25fd3b4225f6f288f1356
Serhan <35522745+SerhanTRGL@users.noreply.github.com>
2024-03-21 08:48:12 +0200

    Small change

--- Assets/Scripts/Player/InputHandlers/RandomInputHandler.cs
+++ Assets/Scripts/Player/InputHandlers/RandomInputHandler.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class RandomInputHandler : InputHandler{
     private void FixedUpdate() {
-        this.JumpInput = Random.Range(0f,1f) < 0.40f;
+        this.JumpInput = Random.Range(0f,1f) < 0.15f;
         this.ActiveAbility1Input = Random.Range(0f,1f) < 0.01f;
         this.ActiveAbility2Input = Random.Range(0f,1f) < 0.01f;
         this.BasicAbilityInput = Random.Range(0f,1f) < 0.05f;
