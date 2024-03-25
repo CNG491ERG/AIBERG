e206ab3536f43c26f9654b5995a159ac53abc0d9
MahradHosseini <mahrad.hosseini@metu.edu.tr>
2024-03-25 18:37:00 +0200

    PlayerAgent Initial
    PlayerAgent.cs, updated Boss.yaml, and BossAndPlayer.yaml files added

--- Assets/Scripts/Player/PlayerAgent.cs
+++ Assets/Scripts/Player/PlayerAgent.cs
@@ -4,21 +4,21 @@ using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
 using UnityEngine;
 
-public class BossAgent : Agent{
-    [Header("References")]
+public class PlayerAgent : Agent{
+	[Header("References")]
     [SerializeField] private Environment environment;
     [SerializeField] private Player player;
     [SerializeField] private Boss boss;
 
-    public override void Initialize() {
+    public override void Initialize(){
         boss = environment.Boss;
         player = environment.Player;
-        environment.Player.OnDamageableDeath += Player_OnDamageableDeath;
+        player.OnDamageableDeath += Player_OnDamageableDeath;
         boss.OnDamageableDeath += Boss_OnDamageableDeath;
         environment.OnMaxStepsReached += Environment_OnMaxStepsReached;
     }
 
-    public override void CollectObservations(VectorSensor sensor) {
+    public override void CollectObservations(VectorSensor sensor){
         sensor.AddObservation(boss.transform.localPosition);
         sensor.AddObservation(boss.Health);
         sensor.AddObservation(boss.basicAttackAbility.CanBeUsed);
@@ -38,48 +38,48 @@ public class BossAgent : Agent{
     }
 
     public override void Heuristic(in ActionBuffers actionsOut) {
-        Debug.Log("Inside Boss heuristic");
-        bool moveDownInput = Input.GetKey(KeyCode.DownArrow);
-        bool moveUpInput = Input.GetKey(KeyCode.UpArrow);
-        bool basicAttackInput = Input.GetKey(KeyCode.J);
-        bool spawnAttackDronesInput = Input.GetKey(KeyCode.K);
+        Debug.Log("Inside Player heuristic");
+        bool basicAttackInput = Input.GetMouseButton(0);
+        bool activeAbility1Input = Input.GetKey(KeyCode.Q);
+        bool activeAbility2Input = Input.GetKey(KeyCode.E);
+        bool jumpInput = Input.GetKey(KeyCode.Space);
 
         var discreteActionsOut = actionsOut.DiscreteActions;
         
         discreteActionsOut[0] = 0; //No movement
-        discreteActionsOut[0] = moveDownInput ? 1 : discreteActionsOut[0];
-        discreteActionsOut[0] = moveUpInput ? 2 : discreteActionsOut[0];
+        discreteActionsOut[0] = jumpInput ? 1 : discreteActionsOut[0];
         discreteActionsOut[1] = 0; //No attack
         discreteActionsOut[1] = basicAttackInput ? 1 : discreteActionsOut[1];
-        discreteActionsOut[1] = spawnAttackDronesInput ? 2 : discreteActionsOut[1];
+        discreteActionsOut[1] = activeAbility1Input ? 2 : discreteActionsOut[1];
+        discreteActionsOut[1] = activeAbility2Input ? 3 : discreteActionsOut[1];
     }
 
-
     private void Boss_OnDamageableDeath(object sender, EventArgs e){
-        float reward = (float)(-0.5 - (player.Health/player.MaxHealth) * 0.5);
+        float reward = (float)(0.5 + (player.Health/player.MaxHealth) * 0.5);
         AddReward(reward);
         EndEpisode();
     }
 
     private void Player_OnDamageableDeath(object sender, EventArgs e){
-        float reward = (float)(0.5 + (boss.Health/boss.MaxHealth) * 0.5);
+        float reward = (float)(-0.5 - (boss.Health/boss.MaxHealth) * 0.5);
         AddReward(reward);
         EndEpisode();
     }
+
     private void Environment_OnMaxStepsReached(object sender, EventArgs e){
         EndEpisode();    
     }
-
 
     public override void OnActionReceived(ActionBuffers actions) {
         int moveAction = actions.DiscreteActions[0];
         int attackAction = actions.DiscreteActions[1];
 
-        Debug.Log("Boss Move Action: " + moveAction);
-        Debug.Log("Boss Attack Action: " + attackAction);
-        boss.moveDownAbility.UseAbility(moveAction == 1);
-        boss.moveUpAbility.UseAbility(moveAction == 2);
-        boss.basicAttackAbility.UseAbility(attackAction == 1);
-        boss.spawnAttackDroneAbility.UseAbility(attackAction == 2);
+        Debug.Log("Player Move Action: " + moveAction);
+        Debug.Log("Player Attack Action: " + attackAction);
+        player.jump.UseAbility(moveAction == 1);
+        player.basicAbility.UseAbility(attackAction == 1);
+        player.activeAbility1.UseAbility(attackAction == 2);
+        player.activeAbility2.UseAbility(attackAction == 3);
     }
 }
+
