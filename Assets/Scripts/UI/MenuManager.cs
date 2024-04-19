97c6abc0deb1c783d2242113eb6eff5f12a4e564
Eren <149137601+erenyararr@users.noreply.github.com>
2024-04-19 21:23:16 +0300

    added menu manager

--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -1,15 +1,41 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-namespace AIBERG.UI{
-public class MenuManager : MonoBehaviour{
+namespace AIBERG.UI
+{
+    public class MenuManager : MonoBehaviour
+    {
+        public GameObject mainMenuObject;
+        public GameObject loginMenuObject;
+        public GameObject registerMenuObject;
+        public GameObject gameMenuObject;
+        public GameObject currentMenu;
+        public GameObject previousMenu;
+        private void Start() {
+            currentMenu = mainMenuObject;
+            currentMenu.SetActive(true);
+        }
+        public void LoadScene(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+        public void SwitchToLoginMenu(){
+            previousMenu = currentMenu;
+            currentMenu.SetActive(false);
+            currentMenu = loginMenuObject;
+            currentMenu.SetActive(true);
+        }
+        public void SwitchToRegisterMenu(){
+            previousMenu = currentMenu;
+            currentMenu.SetActive(false);
+            currentMenu = registerMenuObject;
+            currentMenu.SetActive(true);
+        }
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
 
-    public void LoadScene(string sceneName){
-        SceneManager.LoadScene(sceneName);
     }
-    public void QuitGame(){
-        Application.Quit();
-    }
-}
 }
 
